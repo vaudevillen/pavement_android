ed2c3f11655786e1fdb08e196ee375faf4a00139
J Shin <jjshin85@gmail.com>
2016-02-28 17:06:26 -0600

    add permissions check and request

--- app/src/main/java/com/pnpc/pavement/MainActivity.java
+++ app/src/main/java/com/pnpc/pavement/MainActivity.java
@@ -1,36 +1,68 @@
 package com.pnpc.pavement;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
+    private static final String[] INITIAL_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_CONTACTS
+    };
+
+    private static final String[] LOCATION_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private static final int LOCATION_REQUEST = 1337;
     Button serviceButton;
+    private TextView locationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        locationView=(TextView)findViewById(R.id.location_view);
+
+        updateLocationView();
+        if (hasLocationPermission() == false){
+            requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
+        }
         serviceButton = (Button) findViewById(R.id.service_button);
 
         serviceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent serviceIntent = new Intent(MainActivity.this, PavementService.class);
-                if (serviceButton.getText() == "Start Service"){
+                if (serviceButton.getText() == "Start Service") {
                     startService(serviceIntent);
                     serviceButton.setText("Stop Service");
                     Log.d("", "startService called");
-                }
-                else
-                {
+                } else {
                     stopService(serviceIntent);
                     serviceButton.setText("Start Service");
                 }
@@ -66,5 +98,25 @@ public class MainActivity extends Activity {
         super.onPause();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == LOCATION_REQUEST){
+            if(hasLocationPermission()){
+                updateLocationView();
+            }
+        }
+    }
+    private boolean hasLocationPermission() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.GINGERBREAD){
+            return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+        }
+        else{
+            return true;
+        }
+    }
+    private void updateLocationView(){
+        locationView.setText(String.valueOf(hasLocationPermission()));
+    }
 
 }
